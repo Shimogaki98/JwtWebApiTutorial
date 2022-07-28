403be6b110b768980b6f1c346226f46a2e6675a8
MarcelloBronzatti <mbsimogaki@gmail.com>
2022-07-27 22:52:43 -0300

    Add Role-Based Authorization.

--- JwtWebApiTutorial/Controllers/AuthController.cs
+++ JwtWebApiTutorial/Controllers/AuthController.cs
@@ -47,21 +47,21 @@ namespace JwtWebApiTutorial.Controllers
 
         private string CreateToken(User user)
         {
-
             List<Claim> claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.UserName)
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Role,"Admin")
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
                 _configuration.GetSection("AppSettings:Token").Value));
 
-            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
             var token = new JwtSecurityToken(
                 claims: claims,
                 expires: DateTime.Now.AddDays(1),
-                signingCredentials: cred);
+                signingCredentials: creds);
 
             var jwt = new JwtSecurityTokenHandler().WriteToken(token);
             return jwt;
